867df46f0a7f635f2c1b34cebc5685d9c61e8ce7
Alexander Innes <github@deborahclerkin.co.uk>
2017-04-01 20:45:39 +0100

    bugfix

--- rtfm.py
+++ rtfm.py
@@ -546,7 +546,7 @@ if __name__ == "__main__":
 	sql = "SELECT hash,URL FROM TblUpdates"
 	cur.execute(sql)
 	dbsversion = cur.fetchall()
-	if dbsversion is None:
+	if not dbsversion:
 		print ANSI["yellow"] + ANSI["bold"] + "[WARNING]: " + ANSI["reset"] + "No DB, please run rtfm -u"
 	parser = optparse.OptionParser(\
 		usage="Usage: %prog [OPTIONS]",
